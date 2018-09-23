1e93bc2ff871528510066b5261004c7b9fa1a057
theinfradev <43518082+theinfradev@users.noreply.github.com>
2018-09-24 05:03:37 +1000

    Update Functions.ps1
    minor updates to clear up some ambiguity,

--- Functions.ps1
+++ Functions.ps1
@@ -2,7 +2,6 @@
 ##   BASIC FUNCTIONS ##
 #######################
 
-
 ##Get service data from a PC ##
 
 #############
@@ -48,16 +47,15 @@ Function Get-ServiceData
     
     $service = "Unable to retrieve data"
     $service = Get-Service -ComputerName $Computername
-    Return $service
+    Return [object]$service
   }
 $Servicedata = Get-ServiceData -ComputerName $ComputerName
 
 <# Attributes of this style.
-    1. $Computername is in scope as we are passing it as a parameter into this function. We have also given ourselves extra cmdlt such as -whatif and -confirm
-    2. Get-service data is added to a variable that is already initialised. If Get-Service fails to retrieve any data our original data will be kept. If it succeeds our data will be overwritten by the object returned.
-    3. If Get-Service fails for any reason - we get an error message.
-    4. Finally $ServiceData is also initialised as 'No data' which is not a null. This also means the variable exists, whereas before it would not have existed at all. This stops errors further down the line from this variable not existing.
+    1. $Computername is in scope as we are passing it as a parameter into this function. 
+    2. We have also given ourselves extra cmdlt such as -whatif and -confirm
+    3. Finally we declared that the input data is a string. So we have given ourself a type check to make sure we are giving the right type of variable and if not it will cast it as a string.
+    4. Get-service data is added to a variable that is already initialised. If Get-Service fails to retrieve any data our original data will be kept. If it succeeds our data will be overwritten by the object returned.
+    5. If Get-Service fails for any reason - we get an error message.
+    6. Finally $ServiceData is also initialised as a string 'No data' which is not a null. This also means the variable exists, whereas before it would not have existed at all. This stops errors further down the line from this variable not existing.
 #>
-
-#
-#
